98c6530c1b02fe5e375104996faf11797146e680
aruss <arusslabs@gmail.com>
2017-10-05 18:57:08 +0200

    Using config utils

--- src/IdentityBase.Public/Program.cs
+++ src/IdentityBase.Public/Program.cs
@@ -8,24 +8,30 @@ namespace IdentityBase.Public
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging;
+    using ServiceBase;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            IConfiguration config = Program.LoadConfiguration(args);
+            IConfiguration config = ConfigUtils.LoadConfiguration<Startup>(
+                path: "./AppData/config.json",
+                basePath: Directory.GetCurrentDirectory(),
+                args: args
+            );
+
             IConfigurationSection configHost = config.GetSection("Host");
 
             IWebHostBuilder hostBuilder = new WebHostBuilder()
-             .UseKestrel()
-             .UseUrls(configHost.GetValue<string>("Urls"))
-             .UseContentRoot(Directory.GetCurrentDirectory())
-             .UseConfiguration(config)
-             .ConfigureLogging((hostingContext, logging) =>
-             {
-                 logging.AddSerilog(hostingContext.Configuration);
-             })
-             .UseStartup<Startup>();
+                .UseKestrel()
+                .UseUrls(configHost.GetValue<string>("Urls"))
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddSerilog(hostingContext.Configuration);
+                })
+                .UseStartup<Startup>();
 
             if (configHost.GetValue<bool>("UseIISIntegration"))
             {
@@ -36,31 +42,6 @@ namespace IdentityBase.Public
             hostBuilder
                 .Build()
                 .Run();
-        }
-
-        public static IConfiguration LoadConfiguration(string[] args)
-        {
-            bool isDevelopment = Environment
-                .GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
-                .Equals("Development", StringComparison.OrdinalIgnoreCase);
-
-            IConfigurationBuilder configBuilder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("./AppData/config.json",
-                                optional: false,
-                                reloadOnChange: false);
-
-            if (isDevelopment)
-            {
-                configBuilder.AddUserSecrets<Startup>();
-            }
-
-            IConfiguration config = configBuilder
-                .AddEnvironmentVariables()
-                .AddCommandLine(args)
-                .Build();
-
-            return config;
         }
     }
 }
