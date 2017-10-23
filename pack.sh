8fe994d10ff6452ba5e5c38e66992c0a21231f4b
aruss <arusslabs@gmail.com>
2017-10-23 17:00:19 +0200

    Cleaned up nuget packages

--- pack.sh
+++ pack.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-nuget pack ./src/IdentityBase/IdentityBase.nuspec -OutputDirectory ./artifacts/packages -version $APPVEYOR_BUILD_VERSION
-nuget pack ./src/IdentityBase.Public/IdentityBase.Public.nuspec -OutputDirectory ./artifacts/packages -version $APPVEYOR_BUILD_VERSION
-nuget pack ./src/IdentityBase.Public.EntityFramework/IdentityBase.Public.EntityFramework.nuspec -OutputDirectory ./artifacts/packages -version $APPVEYOR_BUILD_VERSION
+nuget pack ./src/IdentityBase.Public/IdentityBase.nuspec -OutputDirectory ./artifacts/packages -version $APPVEYOR_BUILD_VERSION
