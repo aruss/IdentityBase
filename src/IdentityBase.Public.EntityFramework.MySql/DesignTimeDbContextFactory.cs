d15db29f5650192e095e729abe6f352da0bdce4e
aruss <arusslabs@gmail.com>
2017-10-26 12:14:19 +0200

    Added MySql db provider

---
+++ src/IdentityBase.Public.EntityFramework.MySql/DesignTimeDbContextFactory.cs
@@ -0,0 +1,37 @@
+#if DEBUG
+
+namespace IdentityBase.Public.EntityFramework.MySql
+{
+    using System.Reflection;
+    using IdentityBase.Public.EntityFramework.Options;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+
+    /// <summary>
+    /// DesignTimeDbContextFactory required for the EF Migration tools 
+    /// </summary>
+    public class DesignTimeDbContextFactory :
+        IDesignTimeDbContextFactory<MigrationDbContext>
+    {
+        public MigrationDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<MigrationDbContext> dbBuilder
+                = new DbContextOptionsBuilder<MigrationDbContext>();
+
+            string connString =
+                "Server=localhost;database=identitybase;uid=root;pwd=root;";
+            
+            string migrationsAssembly = typeof(DesignTimeDbContextFactory)
+                .GetTypeInfo().Assembly.GetName().Name;
+
+            dbBuilder.UseMySql(connString,
+                o => o.MigrationsAssembly(migrationsAssembly));
+
+            var options = new EntityFrameworkOptions();
+
+            return new MigrationDbContext(dbBuilder.Options, options);
+        }
+    }
+}
+
+#endif
