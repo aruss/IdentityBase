b6c4514174184bad01cc7f88634f421bfd89aa3c
aruss <arusslabs@gmail.com>
2017-11-02 02:53:00 +0100

    Updated IdentityServer4

--- src/IdentityBase.Public/Api/Invitations/InvitationsPutInputModel.cs
+++ src/IdentityBase.Public/Api/Invitations/InvitationsPutInputModel.cs
@@ -4,32 +4,32 @@ namespace IdentityBase.Public.Api.Invitations
     using System.ComponentModel.DataAnnotations;
 
     /// <summary>
-    /// Represents input message for creating invitation
+    /// Represents input message for creating invitation.
     /// </summary>
     public class InvitationsPutInputModel
     {
         /// <summary>
-        /// Email address of a invited user
+        /// Email address of a invited user.
         /// </summary>
         [Required]
         [EmailAddress]
         [StringLength(254)]
         public string Email { get; set; }
+        
+        /// <summary>
+        /// UserAccount id of the user who creates a invitation.
+        /// </summary>
+        public Guid? InvitedBy { get; set; }
 
         /// <summary>
-        /// Client id of the application where the user gets redirected
+        /// Client id of the application where the user gets redirected.
         /// </summary>
         [Required]
         public string ClientId { get; set; }
 
         /// <summary>
-        /// UserAccount id of the user who creates a invitation
-        /// </summary>
-        public Guid? InvitedBy { get; set; }
-    
-        /// <summary>
-        /// Return URI is used to redirect back to client, must be one of the clients 
-        /// white listed URIs
+        /// Return URI is used to redirect back to client, must be one of the
+        /// clients white listed URIs.
         /// </summary>
         public string ReturnUri { get; set; }
     }
