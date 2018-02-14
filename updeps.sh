050ab3bf1cfef1b7c9f3675ff79a7ad50851eec9
aruss <arusslabs@gmail.com>
2018-02-14 14:41:15 +0100

    Added updeps tool

---
+++ updeps.sh
@@ -0,0 +1,12 @@
+./tools/updeps.exe \
+	./src/IdentityBase/IdentityBase.csproj \
+	./src/IdentityBase.EntityFramework/IdentityBase.EntityFramework.csproj \
+	./src/IdentityBase.EntityFramework.MySql/IdentityBase.EntityFramework.MySql.csproj \
+	./src/IdentityBase.EntityFramework.Npgsql/IdentityBase.EntityFramework.Npgsql.csproj \
+	./src/IdentityBase.EntityFramework.SqlServer/IdentityBase.EntityFramework.SqlServer.csproj \
+	./src/IdentityBase.SendGrid/IdentityBase.SendGrid.csproj \
+	./src/IdentityBase.Shared/IdentityBase.Shared.csproj \
+	./src/IdentityBase.Smtp/IdentityBase.Smtp.csproj \
+	./src/IdentityBase.Twilio/IdentityBase.Twilio.csproj \
+	./src/IdentityBase.WebApi/IdentityBase.WebApi.csproj \
+	./src/IdentityBase/IdentityBase.nuspec
