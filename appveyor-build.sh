bd90830fe62fe7ad210cc2661c2a874a9f6068ff
aruss <arusslabs@gmail.com>
2018-06-15 17:57:03 +0200

    ..

--- appveyor-build.sh
+++ appveyor-build.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-echo "install required tools"
-sudo apt-get update
-sudo apt-get --assume-yes install zip
-
 echo "build windows version"
 sh build.sh win7-x64 $APPVEYOR_BUILD_VERSION
 
-echo "pack windows release files: $DIR/build/win7-x64/identitybase-$APPVEYOR_BUILD_VERSION -> $DIR/build/identitybase-$APPVEYOR_BUILD_VERSION.zip"
+echo "pack windows release files: $APPVEYOR_BUILD_FOLDER/build/win7-x64/identitybase-$APPVEYOR_BUILD_VERSION -> $APPVEYOR_BUILD_FOLDER/build/identitybase-$APPVEYOR_BUILD_VERSION.zip"
 cd ./build/win7-x64/ && zip -rq ../identitybase-$APPVEYOR_BUILD_VERSION.zip ./identitybase-$APPVEYOR_BUILD_VERSION && cd -
-#appveyor PushArtifact $DIR/build/identitybase-$APPVEYOR_BUILD_VERSION.zip
 
 echo "build linux version"
 sh build.sh linux-x64 $APPVEYOR_BUILD_VERSION
 
-echo "pack linux release files: $DIR/build/linux-x64/identitybase-$APPVEYOR_BUILD_VERSION -> $DIR/build/identitybase-$APPVEYOR_BUILD_VERSION.tgz"
-tar -czf $DIR/build/identitybase-$APPVEYOR_BUILD_VERSION.tgz $DIR/build/linux-x64/identitybase-$APPVEYOR_BUILD_VERSION
-#appveyor PushArtifact $DIR/build/identitybase-$APPVEYOR_BUILD_VERSION.tgz
+echo "pack linux release files: $APPVEYOR_BUILD_FOLDER/build/linux-x64/identitybase-$APPVEYOR_BUILD_VERSION -> $APPVEYOR_BUILD_FOLDER/build/identitybase-$APPVEYOR_BUILD_VERSION.tgz"
+tar -czf $APPVEYOR_BUILD_FOLDER/build/identitybase-$APPVEYOR_BUILD_VERSION.tgz $APPVEYOR_BUILD_FOLDER/build/linux-x64/identitybase-$APPVEYOR_BUILD_VERSION
 
 echo "build docker image"
-cp $DIR/distribution/Dockerfile $DIR/build/linux-x64
-docker build $DIR/build/linux-x64 --build-arg VERSION=$APPVEYOR_BUILD_VERSION -t identitybasenet/identitybase:$APPVEYOR_BUILD_VERSION
+cp $APPVEYOR_BUILD_FOLDER/distribution/Dockerfile $APPVEYOR_BUILD_FOLDER/build/linux-x64
+docker build $APPVEYOR_BUILD_FOLDER/build/linux-x64 --build-arg VERSION=$APPVEYOR_BUILD_VERSION -t identitybasenet/identitybase:$APPVEYOR_BUILD_VERSION
--- appveyor-deploy.sh
+++ appveyor-deploy.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
 echo "publish docker image"
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker push identitybasenet/identitybase:$APPVEYOR_BUILD_VERSION
 
 echo "create GitHub release"
-
-echo "debug -------------------------------------------"
-echo "DIR = $DIR"
-ls $DIR/tools -l
-ls $DIR/build -l
-echo "debug -------------------------------------------"
-
-sudo $DIR/tools/ghr -t "$GITHUB_TOKEN" -u "IdentityBaseNet" -r "IdentityBase" -c "$APPVEYOR_REPO_COMMIT" -delete -draft v$APPVEYOR_BUILD_VERSION $DIR/build
+cd $APPVEYOR_BUILD_FOLDER/tools && ./ghr -t "$GITHUB_TOKEN" -u "IdentityBaseNet" -r "IdentityBase" -c "$APPVEYOR_REPO_COMMIT" -delete -draft v$APPVEYOR_BUILD_VERSION $APPVEYOR_BUILD_FOLDER/build && cd -
