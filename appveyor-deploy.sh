87335ef0f822f3a5cb181080a444b6ad18d359a9
aruss <arusslabs@gmail.com>
2018-06-15 16:41:23 +0200

    Doing appveyor build scripts: trying my best, ok

--- appveyor-deploy.sh
+++ appveyor-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# move files to git releases
-# publish docker image
+DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-#docker login -u="$DOCKER_USER" -p="$eDOCKER_PASS"
-#docker push identitybasenet/identitybase:$APPVEYOR_BUILD_VERSION
+echo "publish docker image"
+docker login -u="$DOCKER_USER" -p="$eDOCKER_PASS"
+docker push identitybasenet/identitybase:$APPVEYOR_BUILD_VERSION
 
 echo "create GitHub release"
-sudo ./tools/ghr -t "$GITHUB_TOKEN" -u "IdentityBaseNet" -r "IdentityBase" -c "$APPVEYOR_REPO_COMMIT" -delete -draft v$APPVEYOR_BUILD_VERSION ./build
+sudo $DIR/tools/ghr -t "$GITHUB_TOKEN" -u "IdentityBaseNet" -r "IdentityBase" -c "$APPVEYOR_REPO_COMMIT" -delete -draft v$APPVEYOR_BUILD_VERSION $DIR/build
