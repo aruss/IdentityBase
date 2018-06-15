eb9d1f3067494cecb9e25f108d747c31e5bbcb72
aruss <arusslabs@gmail.com>
2018-06-15 13:04:01 +0200

    D'oh!

--- appveyor-clone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git clone -q --branch=$APPVEYOR_REPO_BRANCH https://github.com/$APPVEYOR_REPO_NAME.git $APPVEYOR_BUILD_FOLDER
-git checkout -qf $APPVEYOR_REPO_COMMIT
-git submodule update --init
