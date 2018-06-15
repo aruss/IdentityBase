9793fccfdc3cbce7862416e2f2813b4d1d1c9206
aruss <arusslabs@gmail.com>
2018-06-15 17:00:30 +0200

    no message

--- appveyor-deploy.sh
+++ appveyor-deploy.sh
@@ -3,8 +3,15 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 echo "publish docker image"
-docker login -u="$DOCKER_USER" -p="$eDOCKER_PASS"
+docker login --username=$DOCKER_USER --password=$DOCKER_PASS
 docker push identitybasenet/identitybase:$APPVEYOR_BUILD_VERSION
 
 echo "create GitHub release"
+
+echo "debug -------------------------------------------"
+echo "DIR = $DIR"
+cd ls $DIR/tools -l
+cd ls $DIR/build -l
+ehco "debug -------------------------------------------"
+
 sudo $DIR/tools/ghr -t "$GITHUB_TOKEN" -u "IdentityBaseNet" -r "IdentityBase" -c "$APPVEYOR_REPO_COMMIT" -delete -draft v$APPVEYOR_BUILD_VERSION $DIR/build
